b63178c678256951ca8bf6068dab0b981770c773
gh0stwizard <vitaliy.tokarev@gmail.com>
2020-06-27 10:22:02 +0300

    update version to 1.1.1.6

--- Main.cs
+++ Main.cs
@@ -23,37 +23,58 @@ namespace ConsoleApplication2
 
             input.Read(actualGuid, 0, guidSize);
             input.Read(version, 0, intSize);
-            
+
+#if false
+            guid = new Guid(actualGuid);
+            Console.WriteLine("GUID: {0}", guid.ToString());
+            Console.Write("Version:");
+            for (var i = 0; i < intSize; i++)
+            {
+                Console.Write(" {0:X}", version[i]);
+            }
+            Console.WriteLine("");
+#endif
+
             GZipStream gzipStream = new GZipStream(input, CompressionMode.Decompress);
 
             gzipStream.CopyTo(output);
             output.Flush();
         }
+
+
         static void Pack(Stream input, Stream output)
         {
             // Obtained from inspecting a global.dat file
             byte[] versionBytes = {
-                                      // Guid
-                                       0xF9, 0x53, 0x8B, 0x83, 0x1F, 0x36, 0x32, 0x43, 0xBA, 0xAE, 0x0D, 0x17, 0x86, 0x5D, 0x08, 0x54,
-                                       // Version
-                                       0x41, 0xF1, 0x4C, 0x31, 0x41, 0x00, 0x00, 0x00
-                                  };
+                // Guid
+                0xF9, 0x53, 0x8B, 0x83, 0x1F, 0x36, 0x32, 0x43, 0xBA, 0xAE, 0x0D, 0x17, 0x86, 0x5D, 0x08, 0x54,
+                // Version
+                0x2c, 0x4a, 0x94, 0x29, 0x4f, 0x00, 0x00, 0x00
+            };
             output.Write(versionBytes, 0, versionBytes.Length);
             GZipStream gzipStream = new GZipStream(output, CompressionMode.Compress);
             input.CopyTo(gzipStream);
             gzipStream.Flush();
             gzipStream.Close();
         }
+
+
         static void Main(string[] args)
         {
             string compressOption = args[0];
             Stream stdin = System.Console.OpenStandardInput();
             Stream stdout = System.Console.OpenStandardOutput();
-            if (compressOption == "pack") {
+
+            if (compressOption == "pack")
+            {
                 Pack(stdin, stdout);
-            } else if (compressOption == "unpack") {
+            }
+            else if (compressOption == "unpack")
+            {
                 Unpack(stdin, stdout);
-            } else {
+            }
+            else
+            {
                 throw new Exception("Unknown compression option");
             }
         }
